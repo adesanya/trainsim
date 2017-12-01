aa75ef44ed701620745947ff171504e10d5622f1
olaoluwa Adesanya <olaolu961@gmail.com>
2017-12-01 01:08:05 -0800

    Added class

--- train.py
+++ train.py
@@ -1,4 +1,13 @@
 
+class Train:
+	def __init__(self, ID,station):
+		self.trainid=ID
+		self.station=station
+		self.passangers=0
+
+	def addPassangers(self, pcount):
+		self.passangers+=pcount
+
 
 if __name__=="__main__":
 	print("Starting Simulation")
